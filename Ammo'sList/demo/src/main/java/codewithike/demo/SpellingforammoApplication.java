87896d2275a8300c98168f889501eb17bc2f904b
Isaac Mills-Owoo <isaac@Isaacs-MacBook-Pro.local>
2025-05-19 17:42:48 -0400

    initiallyset up

---
+++ Ammo'sList/demo/src/main/java/codewithike/demo/SpellingforammoApplication.java
@@ -0,0 +1,13 @@
+package codewithike.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellingforammoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpellingforammoApplication.class, args);
+	}
+
+}
---
+++ Ammo'sList/demo/src/test/java/codewithike/demo/SpellingforammoApplicationTests.java
@@ -0,0 +1,13 @@
+package codewithike.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpellingforammoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
